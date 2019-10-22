ce7003cd6af2d0279b53d4a5d01acb5c2adf93be
waldeyr <waldeyr@gmail.com>
2019-10-22 08:08:39 -0300

    Lista duplamente encadeada

--- Code/Listas/ListaEncadeada03.c
+++ Code/Listas/ListaEncadeada03.c
@@ -4,7 +4,7 @@
 
 // Define o tipo No contendo
 typedef struct No{
-    struct No *anteriorimo;
+    struct No *anterior;
     struct No *anterior;
     int dado;
 } No;
@@ -18,11 +18,11 @@ void inserir(int dado) {
     no->dado = dado;
     if (cabeca == NULL){//lista vazia
         cabeca = no;
-        no->anteriorimo = NULL;
+        no->anterior = NULL;
         no->anterior = NULL;        
     }
     else {
-        no->anteriorimo = cabeca;
+        no->anterior = cabeca;
         cabeca->anterior = no;
         no->anterior = NULL;
         cabeca = no;
@@ -37,8 +37,8 @@ void imprimirLista() {
     }
     No* no = cabeca;
     while (no != NULL) {
-        printf("|%p|%d(%p)|%p|\t", no->anterior, no->dado, no, no->anteriorimo);
-        no = no->anteriorimo;
+        printf("|%p|%d(%p)|%p|\t", no->anterior, no->dado, no, no->anterior);
+        no = no->anterior;
     }
     printf("\n");return;
 }
