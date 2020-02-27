142e8108cb9e76cd35f1127ab5a0ceb7a9aa5249
waldeyr <waldeyr@gmail.com>
2020-02-27 14:01:10 -0300

    Trabalhando com variáveis;

--- Code/Basics/prog005.c
+++ Code/Basics/prog005.c
@@ -2,6 +2,6 @@
 
 void main() {
     int n1 = 1, n2 = 2;
-    int n3 = n1 + n2;
-    printf("%d + %d = %d \n", n1, n2, n3);
+    int soma = n1 + n2;
+    printf("%d + %d = %d \n", n1, n2, soma);
 }
--- Code/Basics/prog007.c
+++ Code/Basics/prog007.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 void main() {
-    int n;
-    printf("Give an integer number...: ");
-    scanf("%d", &n);
-    printf("The supplied number was %d \n", n);
+    float pi = 3.1415;
+    printf("Pi = %.2f\n", pi);
     
 }
